7e921b851782549b0b6b1d38c5525318ce5a279a
Polgár András <azazdeaz@gmail.com>
2015-11-15 22:56:47 +0100

    wip: refactoring to quick-interface

--- src/QuickInterface.jsx
+++ src/QuickInterface.jsx
@@ -1,10 +1,86 @@
 import React from 'react'
-import Item from './Item'
+import Row from './Row'
+import shallowEqual from 'react-pure-render/shallowEqual'
+
+import React, { PropTypes } from 'react'
+
+class QuickInterfaceextends React.Component {
+  constructor({createSettings, children, ...options}) {
+    super()
+
+    const settings = props.createSettings(options)
+    this.settings = settings
+
+    this.state = {
+      hover: false,
+      open: settings.hasOwnProperty('open')
+        ? settings.open
+        : settings.hasOwnProperty('defaultOpen')
+        ? settings.defaultOpen
+        : true
+    }
+  }
+
+  componentWillReceiveProps(nextProps) {
+    const {
+      createSettings, nextCreateSettings,
+      children: nextChildren,
+      ...nextOptions
+    } = nextProps
+
+    const {
+      createSettings,
+      children,
+      ...options
+    }
+
+    if (!shallowEqual(options, nextOptions)) {
+      this.settings = nextCreateSettings(nextOptions)
+    }
+  }
+
+  render () {
+    const {settings} = this
+    const {open, hover} = this.state
+    const {children} = this.props
+    const {showChildren} = settings.hasOwnProperty('open')
+      ? settings.open
+      : open
+    const childCount = React.Children.count(children)
+
+    if (settings.ItemComponent) {
+      return <settings.ItemComponent {...this.props}/>
+    }
+
+    const row = settings.hiddenHead ? null : <Row
+      settings = {settings}
+      open = {open}
+      hasChildren = {childCount > 0}
+      onClickOpenToggle = {this.handleClickOpenToggle}/>
+
+    const childrenBlock =  ? null : <div
+      hidden={!open}
+      style={{marginLeft: 4}}>
+      {children}
+    </div>
+
+    return <div
+      hidden = {settings.hidden}
+      style = {{position: 'relative'}}>
+
+      {row}
+
+      <div hidden={!open} style={{marginLeft: 4}}>
+        {children}
+      </div>
+    </div>
+  }
+}
+
+export default QuickInterface
 
 const QuickInterface = ({children, ...rest}) => {
-  return <Item {...rest}>
-    {children}
-  </Item>
+
 }
 
 export default QuickInterface
--- src/Row.jsx
+++ src/Row.jsx
@@ -5,117 +5,108 @@ import DndWrap from './DndWrap'
 import ContextMenuWrap from './ContextMenuWrap'
 import {Icon, Label, getTheme} from 'react-matterkit'
 
-export default class Row extends React.Component {// eslint-disable-line no-shadow
-  constructor(props) {
-    super(props)
-
-    this.state = {
-      hover: false,
-    }
-  }
-
   static contextTypes = {
     matterkitTheme: React.PropTypes.object
   }
 
-  shouldComponentUpdate(nextProps) {
-    return nextProps.settings !== this.props.settings
+export default function Row({
+  createSettings,
+  _stateHover,
+  _stateOpen,
+  ...options
+}) {
+  var {settings, open, hasChildren, onClickOpenToggle} = this.props
+  var {hover} = this.state
+
+  if (settings.Component) {
+    return <settings.Component {...this.props}/>
   }
 
-  render () {
-    var {settings, open, hasChildren, onClickOpenToggle} = this.props
-    var {hover} = this.state
+  var styleConfig = getTheme(this).getStyle('config')
+  var textColor = settings.highlighted ? styleConfig.palette.grey4 : styleConfig.fontColor.normal
 
-    if (settings.Component) {
-      return <settings.Component {...this.props}/>
-    }
-
-    var styleConfig = getTheme(this).getStyle('config')
-    var textColor = settings.highlighted ? styleConfig.palette.grey4 : styleConfig.fontColor.normal
-
-    var styleBlock = {
-      position: 'relative',
-      height: styleConfig.lineHeight,
-      lineHeight: `${styleConfig.lineHeight}px`,
-      display: 'flex',
-      color: settings.highlighted ? styleConfig.palette.blue : styleConfig.palette.grey2,
-      fontSize: '13px',
-      backgroundColor: settings.highlighted ? styleConfig.palette.blue : styleConfig.palette.bg,
-      borderBottom: 'solid 1px #1a1d21',
-      boxSizing: 'border-box',
-    }
-
-    var items = {}
-
-
-    //label
-    if (settings.labels) {
-      items.labels = <span style={{display: 'flex', flex: 1}}>
-        {settings.labels.map((label, id) => {
-          if (typeof label === 'string') {
-            label = {label}
-          }
-          label = {
-            ...label,
-            style: {
-              flex: 1,
-              color: textColor,
-              ...label.style
-            }
-          }
-
-          return <Label key={id} {...label}/>
-        })}
-      </span>
-    }
-
-    //inputs
-    if (settings.inputs) {
-
-      items.inputs = <span style={{display: 'flex', flex: 1}}>
-
-        {settings.inputs.map((inputProps, idx) => {
-          return <Input
-            key = {idx}
-            {...inputProps}/>
-        })}
-      </span>
-    }
-
-    //buttons
-    if (settings.buttons) {
-      items.buttons = <Buttons
-        hover = {hover}
-        buttonStyle = {{color: textColor}}
-        buttons = {settings.buttons}/>
-    }
-
-    //show/hide toggle btn
-    items.toggle = <Icon
-      icon={hasChildren ? (open ? 'chevron-down' : 'chevron-right') : ' '}
-      onClick={hasChildren ? onClickOpenToggle : null}
-      style={{margin: '0 4px', color: textColor}}/>
-
-
-      // <DndWrap
-      //   style={styleBlock}
-      //   onMouseEnter={() => this.setState({hover: true})}
-      //   onMouseLeave={() => this.setState({hover: false})}
-      //   onClick={()=>{
-      //     if (settings.onClick) {
-      //       settings.onClick()
-      //     }
-      //   }}
-      //   draggable = {settings.draggable}>}
-      // </DndWrap>
-    return <ContextMenuWrap options={settings.contextMenu}>
-        <div style={styleBlock}>
-        {items.toggle}
-        {items.labels}
-        {items.inputs}
-        {items.buttons}
-
-        </div>
-    </ContextMenuWrap>
+  var styleBlock = {
+    position: 'relative',
+    height: styleConfig.lineHeight,
+    lineHeight: `${styleConfig.lineHeight}px`,
+    display: 'flex',
+    color: settings.highlighted ? styleConfig.palette.blue : styleConfig.palette.grey2,
+    fontSize: '13px',
+    backgroundColor: settings.highlighted ? styleConfig.palette.blue : styleConfig.palette.bg,
+    borderBottom: 'solid 1px #1a1d21',
+    boxSizing: 'border-box',
   }
+
+  var items = {}
+
+
+  //label
+  if (settings.labels) {
+    items.labels = <span style={{display: 'flex', flex: 1}}>
+      {settings.labels.map((label, id) => {
+        if (typeof label === 'string') {
+          label = {label}
+        }
+        label = {
+          ...label,
+          style: {
+            flex: 1,
+            color: textColor,
+            ...label.style
+          }
+        }
+
+        return <Label key={id} {...label}/>
+      })}
+    </span>
+  }
+
+  //inputs
+  if (settings.inputs) {
+
+    items.inputs = <span style={{display: 'flex', flex: 1}}>
+
+      {settings.inputs.map((inputProps, idx) => {
+        return <Input
+          key = {idx}
+          {...inputProps}/>
+      })}
+    </span>
+  }
+
+  //buttons
+  if (settings.buttons) {
+    items.buttons = <Buttons
+      hover = {hover}
+      buttonStyle = {{color: textColor}}
+      buttons = {settings.buttons}/>
+  }
+
+  //show/hide toggle btn
+  items.toggle = <Icon
+    icon={hasChildren ? (open ? 'chevron-down' : 'chevron-right') : ' '}
+    onClick={hasChildren ? onClickOpenToggle : null}
+    style={{margin: '0 4px', color: textColor}}/>
+
+
+    // <DndWrap
+    //   style={styleBlock}
+    //   onMouseEnter={() => this.setState({hover: true})}
+    //   onMouseLeave={() => this.setState({hover: false})}
+    //   onClick={()=>{
+    //     if (settings.onClick) {
+    //       settings.onClick()
+    //     }
+    //   }}
+    //   draggable = {settings.draggable}>}
+    // </DndWrap>
+  return <ContextMenuWrap options={settings.contextMenu}>
+      <div style={styleBlock}>
+      {items.toggle}
+      {items.labels}
+      {items.inputs}
+      {items.buttons}
+
+      </div>
+  </ContextMenuWrap>
 }
